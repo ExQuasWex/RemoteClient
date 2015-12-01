05324276ae311623546edfe14c2c4a3d344bf25c
Didoy <villerdex@gmail.com>
2015-12-01 23:36:37 +0800

    update staffinfo

--- src/View/ClientWindow/ClientWindow.java
+++ src/View/ClientWindow/ClientWindow.java
@@ -14,7 +14,6 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 import javafx.scene.control.Button;
 
 import java.awt.*;
